c3a08ccd324ac5a4a0bfb8b45933157c7ed0780c
shrutimkrishnan <shruti@powrofyou.com>
2025-03-27 06:30:44 +0000

    updating text and drop down

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,14 @@ st.set_page_config(layout="wide")
 # Read the CSV file from the public S3 URL
 data = pd.read_csv("https://behaviorally-testing.s3.amazonaws.com/sankey_relevant_session_v2.csv")
 
-st.title("Behaviorally Sankey beh_v105")
+st.title("Behaviorally Sankey beh_v107 updated")
 st.write("Sankey Diagram of Participant Journeys")
 
 # App selection dropdown
 app_names = {
     'com.ss.android.ugc.trill': 'TikTok',
-    'com.shopee.id': 'Shopee'
+    'com.shopee.ph': 'Shopee',
+    'com.lazada.android': 'Lazada'
 }
 selected_app = st.selectbox("Select App", options=list(app_names.keys()), format_func=lambda x: app_names[x])
 
